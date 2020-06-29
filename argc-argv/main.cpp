be4ca5011c89125b13d16cffdec1d49639a8f724
Lu Gao <lgao72@gatech.edu>
2020-06-28 17:43:50 -0700

    more details in argv

--- argc-argv/main.cpp
+++ argc-argv/main.cpp
@@ -1,35 +1,51 @@
+#include <cstring>
 #include <iostream>
-#include <cstdio>
 
 using namespace std;
 
+const size_t msgLen = 1024; 
+
 int main(int argc, char** argv) {
 
-    /*
-    char text[] = "a string of characters";
-    string textStr = "a string of characters";
-    string textStr2 = text;
-    cout << "text = " << text << endl;
-    cout << "&text = " << &text << endl;
-
-    char thridchar = text[3];
-    char thridchar2 = *(text + 3);
-    cout << "thridchar = " << thridchar << endl;
-    cout << "thridchar2 = " << thridchar2 << endl;
-
-    cout << "textStr = " << textStr << endl;
-    cout << "&textStr = " << &textStr << endl;
-    cout << "textStr2 = " << textStr2 << endl;
-    cout << "&textStr2 = " << &textStr2 << endl;
-    // not allowed-!
-    // cout << "*(textStr+3) = " << *(textStr + 3) << endl;
-    */
+    // msg is created for string print
+    char msg[msgLen]; // = (char*) malloc(msgLen);
 
     cout << "argc = " << argc << endl;
-    for(int i=0; i<argc; i++) {
-        cout << "\targv[" << i << "] = " << argv[i] << endl;
-    }
+    
+    // print out all the arguments
+    for (int i = 0; i < argc; i++) {
+        memset(msg, 0, msgLen - 1); 
+        sprintf(msg, "\targv[%d] = %s", i, argv[i]);
+        cout << msg << endl;
+    }    
 
-    return 0;
+    cout << "\n----------------let us take a a look at the addresses----------------\n" << endl; 
+    cout << "using this command line to test the code:\n\t./main arg1 arg2 arg3" << endl; 
+
+    cout << "\nthe address of the string array" << endl;
+    cout << "\targv = " << argv << endl; 
+
+    cout << "\nthe first dereference returns the first entry in the array" << endl;
+    cout << "\t*argv = " << *argv << endl; 
+
+    cout << "\nthe second dereference returns the first character of the first entry int the array" << endl;
+    cout << "\t**argv = " << **argv << endl; 
+
+    cout << "\nnotice that the name of the array is a pointer, we apply and offset on it\ne.g. if applying an offset of 3, it will return the 3rd entry of the array" << endl;
+    cout << "\t*(argv + 3) = " << *(argv + 3) << endl; 
+
+    cout << "\nthe address of the first string is equal to argv" << endl; 
+    cout << "\t&argv[0] = " << &argv[0] << endl; 
+
+    cout << "\nif you really want to go there, the dereference of the address first entry\nin the array returns the first entry" << endl; 
+    cout << "\t*(&argv[0]) = " << *(&argv[0]) << endl; 
+
+    cout << "\nthis line is equal to *(argv + 1)" << endl; 
+    cout << "\targv[1] = " << argv[1] << endl;
+
+    cout << "\nfirst character of the first entry in the array" << endl; 
+    cout << "\targv[0][0] = " << argv[0][0] << endl; 
+
+    return EXIT_SUCCESS;
 
 }
