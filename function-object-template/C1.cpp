5ae08f1457aee480dc444ea97b50cf9f60009660
Lu Gao <lgao23@apple.com>
2024-02-27 20:03:00 -0800

    template function in class

---
+++ function-object-template/C1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lgao23 on 2/22/24.
+//
+
+#include "C1.h"
---
+++ function-object-template/C1.h
@@ -0,0 +1,16 @@
+//
+// Created by lgao23 on 2/22/24.
+//
+
+#ifndef FUNCTION_OBJECT_TEMPLATE_C1_H
+#define FUNCTION_OBJECT_TEMPLATE_C1_H
+
+
+
+class C1 {
+
+};
+
+
+
+#endif //FUNCTION_OBJECT_TEMPLATE_C1_H
