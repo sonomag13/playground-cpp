2ab1cc95d60865f405db649ed41d934dccad5c78
Lu Gao <lgao23@apple.com>
2024-02-27 20:03:38 -0800

    function object and template

--- function-object-template/C1.cpp
+++ function-object-template/C1.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "C1.h"
+
+template<class T1, class T2>
+void C1::print(T1 v1, T2 v2) {
+    std::cout << "v1 = " << v1 << std::endl;
+    std::cout << "v2 = " << v2 << std::endl;
+}
+
+template void C1::print<int, int>(int, int);
+template void C1::print<int, float>(int, float);
--- function-object-template/C1.h
+++ function-object-template/C1.h
@@ -2,15 +2,37 @@
 // Created by lgao23 on 2/22/24.
 //
 
-#ifndef FUNCTION_OBJECT_TEMPLATE_C1_H
-#define FUNCTION_OBJECT_TEMPLATE_C1_H
+#pragma once
 
-
+#include <iostream>
 
 class C1 {
+public:
+    C1() = default;
+    C1(int x, float y): x(x), y(y) {}
+    ~C1() = default;
 
+    int printVal(int z) {
+        std::function<void(int, int)> f1 = [this](int x1, int x2)->void{print<int, int>(x1, x2);};
+        f1(z, x);
+
+        std::function<void(int, float)> f2 = [this](int x1, float x2)->void{print<int, float>(x1, x2);};
+        f2(z, y);
+
+        return 0;
+    }
+
+private:
+    int x = 0;
+    float y = 0.0;
+
+     template<class T1, class T2>
+     void print(T1, T2);
+
+//    template<class T1, class T2>
+//    void print(T1 v1, T2 v2) {
+//        std::cout << "v1 = " << v1 << std::endl;
+//        std::cout << "v2 = " << v2 << std::endl;
+//    }
 };
 
-
-
-#endif //FUNCTION_OBJECT_TEMPLATE_C1_H
